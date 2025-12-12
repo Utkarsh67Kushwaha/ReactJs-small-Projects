1a902ccd1b6f0b7c5405dc8d834be6086ba7adee
Utkarsh Kushwaha <utkarshkushwaha67@gmail.com>
2025-12-12 15:22:54 +0530

    fix: correct JSX style object syntax in RightCardContent

--- src/components/Section1/RightCardContent.jsx
+++ src/components/Section1/RightCardContent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+﻿import React from 'react'
 
 const RightCardContent = (props) => {
   return (
@@ -7,7 +7,7 @@ const RightCardContent = (props) => {
             <h2 className='bg-white text-2xl font-semibold rounded-full h-14 w-14 flex justify-center items-center'>{props.id+1}</h2>
             <p className='text-shadow-2xs text-xl leading-relaxed text-white mb-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ipsa veniam magni nam maiores quos.</p>
             <div className='flex gap-4 items-center'>
-                <button style={{backgroundColor:props.color}} className=' text-white font-medium px-8 py-2 rounded-full text-lg '>{props.tag}</button>
+                <button style={{backgroundColor: props.color}} className=' text-white font-medium px-8 py-2 rounded-full text-lg '>{props.tag}</button>
                 <button className=' text-white font-medium px-4 py-2 rounded-full text-lg'><i className="ri-arrow-right-line"></i></button>
             </div>
         </div>
